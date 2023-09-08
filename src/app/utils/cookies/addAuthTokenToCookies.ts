81284859a77ff56f68a2910e66351ea593abce2e
sdaleshin <sdaleshin@gmail.com>
2023-09-08 20:03:53 +0200

    fixed cookie life time

--- src/app/utils/cookies/addAuthTokenToCookies.ts
+++ src/app/utils/cookies/addAuthTokenToCookies.ts
@@ -2,5 +2,7 @@ import Cookies from 'js-cookie'
 import { AUTH_TOKEN_COOKIE_NAME } from '../constants'
 
 export const addAuthTokenToCookies = (jwtToken: string) => {
-    Cookies.set(AUTH_TOKEN_COOKIE_NAME, jwtToken)
+    Cookies.set(AUTH_TOKEN_COOKIE_NAME, jwtToken, {
+        expires: 365,
+    })
 }
--- src/app/utils/cookies/addRefreshTokenToCookies.ts
+++ src/app/utils/cookies/addRefreshTokenToCookies.ts
@@ -2,5 +2,7 @@ import Cookies from 'js-cookie'
 import { REFRESH_TOKEN_COOKIE_NAME } from '../constants'
 
 export const addRefreshTokenToCookies = (refreshToken: string) => {
-    Cookies.set(REFRESH_TOKEN_COOKIE_NAME, refreshToken)
+    Cookies.set(REFRESH_TOKEN_COOKIE_NAME, refreshToken, {
+        expires: 365,
+    })
 }
